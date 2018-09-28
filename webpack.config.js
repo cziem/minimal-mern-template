d95695b2abdeaac2c93846e891ad6a8288b62ccf
Fay Vor <phavorsparks@gmail.com>
2018-09-28 17:30:21 +0100

    Setup up linting rules for eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-entry: './client/src/index.js',
+  entry: './client/src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js'
+  }
 }
