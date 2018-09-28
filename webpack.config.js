728125c17b0274b5fa4838bc6b5db2436a4d4fc3
Fay Vor <phavorsparks@gmail.com>
2018-09-28 17:46:09 +0100

    Fix space linting warning

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel-loader"
+        loader: 'babel-loader'
       },
       {
         test: /\.s?css$/,
